dd3c8d785b8f8bba441d1267524b164c80148291
neliel <skh.unvrs@gmail.com>
2013-02-25 04:47:40 +0100

    makesrc_isnow_multiplatform

--- makesrc.hs
+++ makesrc.hs
@@ -1,20 +1,31 @@
 #!/usr/bin/env runhaskell
 
-{- makesrc.hs | 10 Fév. 2013 à 21h40 | GPLv3 -}
+-- makesrc.hs
+-- Crée le: 10 Fév. 2013 à 21h40
+-- License: GPLv3
 
 import System.Environment (getArgs)
 import System.Cmd         (rawSystem)
 import System.Directory
 import System.FilePath
+import System.Platform
 
+main :: IO ()
 main = do
     [file] <- getArgs
-    let nameFile = takeBaseName file
+    let
+      nameFile = takeBaseName file
     rawSystem "hmake" [nameFile]
     home <- getHomeDirectory
     let
-      nameExe  = nameFile ++ ".exe"
-      homeFile = home ++ "\\.opt\\bin\\" ++ nameExe
+      nameExe  =
+        case os_type of
+          MS_Windows -> nameFile ++ ".exe"
+          Unix       -> nameFile
+      homeFile =
+        case os_type of
+          MS_Windows -> home ++ "\\.opt\\bin\\" ++ nameExe
+          Unix       -> home ++ "/.opt/bin/" ++ nameExe
     copyFile nameExe homeFile
     removeFile nameExe
 --EOF
