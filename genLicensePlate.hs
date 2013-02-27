af2a5e9294803a1f5e41cc205f11e6f7b53907d6
neliel <skh.unvrs@gmail.com>
2013-02-27 18:52:49 +0100

    scr_gen_fini

--- genLicensePlate.hs
+++ genLicensePlate.hs
@@ -24,17 +24,63 @@ genMeSome mask n = do
   glist <- replicateM (n*10) (genFromMask mask)
   return $ take n $ nub glist
 
+writeIt ::  FilePath -> Int -> [String] -> IO ()
+writeIt fi n mask = do
+   glist <- genMeSome mask n
+   writeFile fi $ unlines glist
+
+maj :: String
+maj = ['A'..'Z']
+
+numa :: String
+numa = ['0'..'9']
+
+
 -- | La plage "SS" est interdite en France; le nb de combinaisons limité à 675 324
-genFra :: Int -> IO [String]
-genFra = genMeSome ["S","S","-",['0'..'9'],['0'..'9'],['0'..'9'],"-",['A'..'Z'],['A'..'Z']]
-
+genFra :: [String]
+genFra = ["S","S","-",numa,numa,numa,"-",maj,maj]
 -- | Les voyelles ainsi que les lettres Q sont interdites
-genEsp :: Int -> IO [String]
-genEsp = genMeSome [['0'..'9'],['0'..'9'],['0'..'9'],['0'..'9']," ",['A'..'Z'],"AEIOUYQ",['A'..'Z']]
-
+genEsp :: [String]
+genEsp = [numa,numa,numa,numa," ",maj,"AEIOUYQ",maj]
 -- | La plage allant de 30000 à 49999 n'est pas utilisée
-genDnk :: Int -> IO [String]
-genDnk = genMeSome [['A'..'Z'],['A'..'Z']," ","34",['0','9'],['0','9'],['0','9'],['0','9']]
+genDnk :: [String]
+genDnk = [maj,maj," ","34",numa,numa,numa,numa]
+-- | la lettre Z est utilisée pour identifié une région en random
+genGbr :: [String]
+genGbr = ["Z",maj,numa,numa," ",maj,maj,maj]
+-- | Les allemands ne veulent plus se rappeler du nazisme
+genDeu :: [String]
+genDeu = ["S","S",maj,numa,numa,numa,numa]
+-- | la partie numerique ne peut pas commencer par 0
+genFin :: [String]
+genFin = [maj,maj,maj,"-","0",numa,numa]
+-- | La génération est sequentielle; les Z families vont prendre un peu de temps
+genHun :: [String]
+genHun = ["Z",maj,maj,"-",numa,numa,numa]
+-- | Même topo avec les sigles SD et SS
+genNld :: [String]
+genNld = [numa,"-","S","DS","ZB","-",numa,numa]
+-- | Même topo avec les sigles SD et SS
+genNor :: [String]
+genNor = ["NS","S"," ",numa,numa,numa,numa,numa]
+-- | On introduit des lettres interdites
+genPol :: [String]
+genPol = [maj,maj," ",numa,numa,numa,"BDIOZQ",numa]
+-- | les deux premiéres lettres forment un code region
+genSvn :: [String]
+genSvn = ["ADFHIQTUVWXYZ","ADFHIQTUVWXYZ"," ",maj,numa,"-",numa,numa,numa]
+--- | Les suedois n'utilisent pas la lettre Q
+genSwe :: [String]
+genSwe = [maj,"Q",maj," ",numa,numa,numa]
+-- | Certaines regions n'utilisent aucune des plages libres
+genBra :: [String]
+genBra = ["VWXYZ",maj,maj," ",numa,numa,numa,numa]
+-- | Plaque de la californie qui s'incremente sequentiellement
+genUsa :: [String]
+genUsa = ["9","Z",maj,maj,numa,numa,numa]
+-- |
+genAus :: [String]
+genAus = [maj,maj,maj," ",numa,numa,numa]
 
 main :: IO ()
 main = do
@@ -42,30 +88,22 @@ main = do
   args <- getArgs
   case args of
     (mo:fi:n:_) -> case mo of
-      --"aus" -> writeFile (unlines $ genAus $ read n) fi
-      --"bra" -> writeFile (unlines $ genBra $ read n) fi
-      --"deu" -> writeFile (unlines $ genDeu $ read n) fi
-      "dnk" -> do
-        list <- genDnk $ read n
-        writeFile fi $ unlines list
-      "esp" -> do
-        list <- genEsp $ read n
-        writeFile fi $ unlines list
-      --"fin" -> writeFile (unlines $ genFin $ read n) fi
-      "fra" -> do
-        list <- genFra $ read n
-        writeFile fi $ unlines list
-      --"gbr" -> writeFile (unlines $ genGbr $ read n) fi
-      --"hun" -> writeFile (unlines $ genHun $ read n) fi
-      --"nld" -> writeFile (unlines $ genNld $ read n) fi
-      --"nor" -> writeFile (unlines $ genNor $ read n) fi
-      --"pol" -> writeFile (unlines $ genPol $ read n) fi
-      --"svn" -> writeFile (unlines $ genSvn $ read n) fi
-      --"swe" -> writeFile (unlines $ genSwe $ read n) fi
-      --"usa" -> writeFile (unlines $ genUsa $ read n) fi
+      "aus" -> writeIt fi (read n) genAus
+      "bra" -> writeIt fi (read n) genBra
+      "deu" -> writeIt fi (read n) genDeu
+      "dnk" -> writeIt fi (read n) genDnk
+      "esp" -> writeIt fi (read n) genEsp
+      "fin" -> writeIt fi (read n) genFin
+      "fra" -> writeIt fi (read n) genFra
+      "gbr" -> writeIt fi (read n) genGbr
+      "hun" -> writeIt fi (read n) genHun
+      "nld" -> writeIt fi (read n) genNld
+      "nor" -> writeIt fi (read n) genNor
+      "pol" -> writeIt fi (read n) genPol
+      "svn" -> writeIt fi (read n) genSvn
+      "swe" -> writeIt fi (read n) genSwe
+      "usa" -> writeIt fi (read n) genUsa
       _     -> error "country is not supported"
-    _           -> error "invalid input, format is: genLicensePlate country file number"
-
-
+    _           -> error "wrong input, format is: genLicensePlate country file number"
 
 --EOF
